fae8d1fc89194f80edeee1c49eeae94a6df0eb85
min <minwang.bus@gmail.com>
2021-03-31 18:04:06 -0400

    project 1 done

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+    
+    def index
+        render json: params
+    end
+
+    def show
+        render json: params
+    end
+
+    def create
+        user = User.new(params.require(:user).permit(:name, :email))
+        
+        if user.save
+            render json: user
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        target = User.find(params[:id])
+
+        if target.update(params.require(:user).permit(:name, :email))
+            redirect_to users_url(target)
+        else 
+            render json: target.error.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        target = User.find(params[:id])
+        
+        target.destroy
+        redirect_to users_url
+    end
+
+end
